c2f1831c30ff8fb913aab8a2d493e467e5753868
pcg <panchenguang@qq.com>
2020-01-02 12:00:10 +0800

    compile ok

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -21,10 +21,10 @@ public class BuildVars {
     public static boolean TON_WALLET_STANDALONE = false;
     public static int BUILD_VERSION = 1819;
     public static String BUILD_VERSION_STRING = "5.13.0";
-    public static int APP_ID = 4;
-    public static String APP_HASH = "014b35b6184100b085b0d0572f9b5103";
-    public static String HOCKEY_APP_HASH = "a5b5c4f551dadedc9918d9766a22ca7c";
-    public static String HOCKEY_APP_HASH_DEBUG = "f972660267c948d2b5d04761f1c1a8f3";
+    public static int APP_ID = 974519;
+    public static String APP_HASH = "775ec0faf7721120b06345d6c78153b9";
+    public static String HOCKEY_APP_HASH = "00000000000000000000000000000000";
+    public static String HOCKEY_APP_HASH_DEBUG = "00000000000000000000000000000000";
     //
     public static String SMS_HASH = DEBUG_VERSION ? "O2P2z+/jBpJ" : "oLeq9AcOZkT";
     public static String PLAYSTORE_APP_URL = "https://play.google.com/store/apps/details?id=org.telegram.messenger";
